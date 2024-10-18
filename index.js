559ad8485c0bf9dab74506abe29550a6ee2d9f86
Prabhjot <prabhjotk14@nycstudents.net>
2024-10-18 10:26:19 -0400

    Initial commit: Adding all the files

---
+++ index.js
@@ -0,0 +1,8 @@
+//Prabhjot_Clicker
+// document.getElementById("count-el").innerText = 5;
+
+let count = 0;
+function increment(){
+    count = count + 1;
+    document.getElementById("count-el").innerText = count;
+}
