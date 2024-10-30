8e82a44fb33af24675b1dda25e594753a2c6a002
khalsa232 <prabhjotk14@nycstudents.net>
2024-10-30 00:03:56 -0400

    final

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function increment(){
     count = count + 1;
     document.getElementById("count-el").innerText = count;
 }
+function save() {
+    document.getElementById("save-el").innerText = document.getElementById("count-el").innerText;
+ }
+ 
